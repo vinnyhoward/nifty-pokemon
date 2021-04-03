02ca393ad67a9f4ef1582b2291d6231377e58b98
Vince Howard <vincenguyenhoward@gmail.com>
2021-04-03 00:37:14 -0600

    added CSS/SASS configuration

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import { testUtil, logString } from './test.utils';
+import './styles/global.scss';
 
 logString(testUtil);
+
+function component() {
+    const element: HTMLDivElement = document.createElement('div');
+  
+    element.innerHTML = ['Testing', 'Configuration!!'].join(' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 module.exports = {
@@ -21,10 +23,17 @@ module.exports = {
                 test: /\.ts$/,
                 exclude: /node_modules/,
                 use: {
-                    // without additional settings, this will reference babel.rc
                     loader: 'babel-loader',
                 }
+            },
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             }
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({ title: 'Nifty Pokemon' }),
+        new MiniCssExtractPlugin()
+    ],
 }
